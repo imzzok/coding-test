9210743d89911cc9100a01c72fbf139455774236
서정 <118805355+imzzok@users.noreply.github.com>
2023-09-15 16:50:44 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    return num_list.reverse();
+}
+
+// 테스트를 위한 예제 배열
+const inputArray = [1, 2, 3, 4, 5];
+const reversedArray = solution(inputArray);
+console.log(reversedArray); // [5, 4, 3, 2, 1]
